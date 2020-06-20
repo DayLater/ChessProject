195d4da66148f126a906c084f2abac718cd3d825
Besh <besh98@bk.ru>
2020-06-20 13:54:58 +0400

    Добавлен класс Player Описан IFigure

--- ChessProject/IFigure.cs
+++ ChessProject/IFigure.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ChessProject
 {
-    class IFigure
+    public interface IFigure
     {
+        Point Position { get; set; }
+        Player Player { get; set; }
+        bool IsAlive { get; set; }
+
+        void Move(Point newPosition);
+
+        List<Point> FindPosibleWays();
     }
 }
