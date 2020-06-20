56bbb4f4d620480133477d320a7732b48f4d8737
Besh <besh98@bk.ru>
2020-06-20 15:48:43 +0400

    Добавил тест с врагом

--- ChessProject/Figures/TestElephant.cs
+++ ChessProject/Figures/TestElephant.cs
@@ -10,20 +10,38 @@ namespace ChessProject.Figures
     [TestFixture]
     public class TestElephant
     {
-        Player player = new Player("White");
+        Player whitePlayer = new Player("White");
+        Player blackPlayer = new Player("Black");
+
         IFigure[,] map = new IFigure[8, 8];
         [Test]
-        public void FindPosibleWays()
+        public void FindPosibleWaysWithoutEnemies()
         {
+            Elephant elephant = new Elephant(new Position(5, 1), whitePlayer);
             var expected = new[]
             { 
                 new Position(4,0), new Position(6, 0), new Position(4,2),
                 new Position(6,2), new Position(3,3), new Position(7,3),
                 new Position(2,4), new Position(1,5),  new Position(0,6)
             };
-            var elephant = new Elephant(new Position(5, 1), player);
+
             map[5, 1] = elephant;
             var result = elephant.FindPosibleWays(map);
+            Assert.That(result.ToArray(), Is.EquivalentTo(expected));
+        }
+
+        [Test]
+        public void FindPosibleWaysWithEnemies()
+        {
+            Elephant elephant = new Elephant(new Position(5, 1), whitePlayer);
+            var elephantEnemy = new Elephant(new Position(4, 2), blackPlayer);
+            map[4, 2] = elephantEnemy;
+            var result = elephant.FindPosibleWays(map);
+            var expected = new[]
+            {
+                new Position(4,0), new Position(6, 0), new Position(4,2),
+                new Position(6,2), new Position(7,3),
+            };
             Assert.That(result.ToArray(), Is.EquivalentTo(expected));
         }
     }
