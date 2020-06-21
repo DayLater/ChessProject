2e4aea40fc50fc8b2b72af9721758dd16c840705
Besh <besh98@bk.ru>
2020-06-21 14:37:41 +0400

    Описал немного королеву

--- ChessProject/Figures/Queen.cs
+++ ChessProject/Figures/Queen.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace ChessProject.Figures
 {
-    public class Queen
+    public class Queen : IFigure
     {
+        public Position Position { get; set; }
+        public Player Player { get;  }
+
+        public bool IsAlive { get; set; }
+
+        public Queen(Position position, Player player)
+        {
+            Position = position;
+            Player = player; 
+        }
+
+        public List<Position> FindPosibleWays(IFigure[,] map)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Move(Position newPosition)
+        {
+            Position = newPosition;
+        }
     }
 }
