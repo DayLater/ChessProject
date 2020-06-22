26ba6f09346b9d70d2c3831ad26b9a8d1f4eccd6
Besh <besh98@bk.ru>
2020-06-22 11:37:56 +0400

    Первые изменения на форме. Взаимодействие

--- ChessProject/Game/GameForm.cs
+++ ChessProject/Game/GameForm.cs
@@ -12,6 +12,10 @@ namespace ChessProject
     {
         private readonly GameModel game = new GameModel();
         Button[,] buttons = new Button[8, 8];
+        Button prevButton;
+        Color prevColor;
+        IFigure prevFigure;
+        bool IsClicked; 
 
         public GameForm()
         {
@@ -23,11 +27,39 @@ namespace ChessProject
                 for (int j = 0; j < 8; j++)
                 {
                     if (game.Map[i, j] is IFigure)
-                    {
                         buttons[i, j].Text = game.Map[i,j].Picture;
-                        buttons[i, j].Enabled = true;
-                    }
                 }
+        }
+
+        void SwapBlocks(object sender, EventArgs e)
+        {
+            var pressedButton = sender as Button;
+            IFigure currentFigure = game.Map[pressedButton.Location.Y / 80, pressedButton.Location.X / 80];
+            if (currentFigure != null)
+            {
+                var list = currentFigure.FindPosibleWays(game.Map);
+                foreach (var pos in list)
+                {
+                    buttons[pos.X, pos.Y].BackColor = Color.Yellow;
+                }
+                prevColor = pressedButton.BackColor;
+                pressedButton.BackColor = Color.Green;
+                IsClicked = true;
+            }
+            else
+            {
+                if (IsClicked)
+                {
+                    game.Map[pressedButton.Location.Y / 80, pressedButton.Location.X / 80] =
+                        game.Map[prevButton.Location.Y / 80, prevButton.Location.X / 80];
+                    game.Map[prevButton.Location.Y / 80, prevButton.Location.X / 80] = currentFigure;
+                    pressedButton.Text = prevButton.Text;
+                    prevButton.Text = "";
+                    prevButton.BackColor = prevColor;
+                    IsClicked = false;
+                }
+            }
+            prevButton = pressedButton;
         }
 
         Button MakeButton(int i, int j)
@@ -39,7 +71,7 @@ namespace ChessProject
             button.Size = size;
             button.Font = new Font("Times New Roman", 28F, FontStyle.Regular, GraphicsUnit.Point, 204);
             button.Location = new Point(j * 80, i * 80);
-            button.Enabled = false;
+            button.Click += SwapBlocks;
             Controls.Add(button);
             return button;
         }
